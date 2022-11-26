4002245bf1cf983ad29df41ef2cddc168e1bbd01
Roman Stadlmair <stadlmair@seppmail.at>
2022-11-26 21:44:28 +0100

    remove backup-sm365rules

--- Public/Rules.ps1
+++ Public/Rules.ps1
@@ -283,51 +283,6 @@ function Remove-SM365Rules {
     }        
 }
 
-<#
-.SYNOPSIS
-    Backs up all existing transport rules to individual json files
-.DESCRIPTION
-    Convenience function to perform a backup of all existing transport rules
-.EXAMPLE
-    Backup-SM365Rules -OutFolder "C:\temp"
-#>
-function Backup-SM365Rules
-{
-    [CmdletBinding()]
-    param
-    (
-        [Parameter(
-             Mandatory = $true,
-             HelpMessage = 'Folder in which the backed up configuration will be stored'
-         )]
-        [Alias("Folder")]
-        [String] $OutFolder
-    )
-
-    begin
-    {
-        if (!(Test-SM365ConnectionStatus))
-        { throw [System.Exception] "You're not connected to Exchange Online - please connect prior to using this CmdLet" }
-
-        Write-Information "Connected to Exchange Organization `"$Script:ExODefaultDomain`"" -InformationAction Continue
-    }
-
-    process
-    {
-        if(!(Test-Path $OutFolder))
-        {New-Item $OutFolder -ItemType Directory}
-
-        Get-TransportRule | Foreach-Object{
-            $n = $_.Name
-            $n = $n -replace "[\[\]*\\/?:><`"]"
-
-            $p = "$OutFolder\rule_$n.json"
-            Write-Verbose "Backing up $($_.Name) to $p"
-            ConvertTo-Json -InputObject $_ | Out-File $p
-        }
-    }
-}
-
 if (!(Get-Alias 'Set-SM365rules' -ErrorAction SilentlyContinue)) {
     New-Alias -Name Set-SM365Rules -Value New-SM365Rules
 }
